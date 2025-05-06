0056592bbf07405de26084f25dfdd12f26d960c3
Ivan Laptev <ivan.laptev952@gmail.com>
2025-05-06 23:43:31 +0300

    Add something

--- main.py
+++ main.py
@@ -12,31 +12,66 @@ class Task(BaseModel):
     id: int
     title: str
     content: str
+    author: User
+
+class TaskCreate(BaseModel):
+    title: str
+    content: str
+    author_id: int
+
+users = [
+    {
+        'id': 1,
+        'username': 'Хуесос',
+    },
+    {
+        'id': 2,
+        'username': 'Пиздолиз',
+    }
+]
 
 tasks = [
     {
         'id': 1,
         'title': 'Micropenis',
         'content': 'Yohoho',
+        'author': users[0],
     },
     {
         'id': 2,
         'title': 'Aiaiai',
         'content': 'Sisi',
+        'author': users[1]
     }
 ]
 
 @app.get('/')
-def home() -> str:
+async def home() -> str:
     return 'Hello pidor)'
 
 @app.get('/tasks')
-def get_all_tasks() -> List[Task]:
+async def get_all_tasks() -> List[Task]:
     return [Task(**task) for task in tasks]
 
 @app.get('/tasks/{id}')
-def get_task(id: int) -> Task:
+async def get_task(id: int) -> Task:
     for task in tasks:
         if task['id'] == id:
             return Task(**task)
     raise HTTPException(status_code=404, detail='Task not found')
+
+@app.post('/tasks/add')
+async def add_task(task: TaskCreate) -> Task:
+    author = next((user for user in users if user['id'] == task.author_id), None)
+    if not author:
+        raise HTTPException(404, 'User not found')
+    
+    new_task = {
+        'id': len(tasks)+1,
+        'title': task.title,
+        'content': task.content,
+        'author': author,
+    }
+    tasks.append(new_task)
+
+    return Task(**new_task)
